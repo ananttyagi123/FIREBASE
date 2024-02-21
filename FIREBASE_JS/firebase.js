6ffaaa69d1cad184d3f21d56380d4a321a3b9228
Ananttyagi123 <ananttyagi359@gmail.com>
2024-02-21 20:00:51 +0530

    Learning FireBase

---
+++ FIREBASE_JS/firebase.js
@@ -0,0 +1,7 @@
+const registor=()=>{
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  console.log(email);
+  console.log(password);
+  
+}
